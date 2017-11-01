782e4bbdaa875468dc3d9eef5883eae01acf0cc7
= <=>
2017-11-01 17:08:48 +0800

    'btn2'

--- app/src/main/java/com/test/androidtest/activity/MainActivity.java
+++ app/src/main/java/com/test/androidtest/activity/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends Activity {
 
     @OnClick(R.id.btn2)
     void onBtn2Click() {
-        Toast.makeText(this, "点击btn2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "点击btn", Toast.LENGTH_SHORT).show();
     }
 
     @OnLongClick(R.id.btn2)
